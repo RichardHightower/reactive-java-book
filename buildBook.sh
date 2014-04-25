a346c94d4979551b59cd5dc8301381dcf3568ca6
javaboon <richard.m.hightower@gmail.com>
2014-04-25 02:33:59 -0700

    site

---
+++ buildBook.sh
@@ -0,0 +1,11 @@
+gitbook ebook . 
+gitbook pdf .
+gitbook build .  
+rm -rf dist
+mkdir dist
+mv book.pdf dist/reactiveJavaBook.pdf
+mv book.epub dist/reactiveJavaBook.epub
+mv _book dist/reactiveJavaBook
+
+
+
